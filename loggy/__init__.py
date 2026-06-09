c7bcff8650a5bc6c5cbb1909ac28e7e2598c6b27
Derek Garcia <dcia811@gmail.com>
2026-06-08 18:04:33 -1000

    feat: Expose default logging level

--- loggy/__init__.py
+++ loggy/__init__.py
@@ -6,12 +6,13 @@ Description: Public facing methods for loggy
 @author Derek Garcia
 """
 from ._levels import Level
-from ._logger import Logger
+from ._logger import Logger, DEFAULT_LOG_LEVEL
 from .util import Timer, format_time
 
 _logger = Logger()
 
 set_log_level = _logger.set_log_level
+
 debug_info = _logger.debug_info
 debug_warn = _logger.debug_warn
 debug_error = _logger.debug_error
@@ -20,10 +21,10 @@ info = _logger.info
 warn = _logger.warn
 error = _logger.error
 
+fatal = _logger.fatal
+
 async_data_queue = _logger.async_data_queue
 threaded_data_queue = _logger.threaded_data_queue
 manual_data_queue = _logger.manual_data_queue
 
-fatal = _logger.fatal
-
-__all__ = ['Level', 'Timer', 'format_time']
+__all__ = ['Level', 'Timer', 'format_time', 'DEFAULT_LOG_LEVEL']
